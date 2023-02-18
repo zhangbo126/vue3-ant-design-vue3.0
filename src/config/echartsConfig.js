cc9a303216c11615ea9eb41bf7507ec06af23d03
ZHANG_666 <1259558183@qq.com>
2023-02-18 23:21:13 +0800

    内容调整

--- src/config/echartsConfig.js
+++ src/config/echartsConfig.js
@@ -23,9 +23,9 @@ export const GEO_3D_OPTIONS = {
 		// }], false),
 		environment: drawAssetsImage('earth_geo_2.jpg'),
 		// environment: [ { itemStyle: { normal: { color: '#fff', shadowBlur: 2, shadowColor: '#fff', opacity: 0.5 } } }, { type: 'image', repeat: 'no-repeat', url: 'https://www.your-url.com/image.jpg', width: 100, height: 100, left: 0, top: 0, z: -2 }, { type: 'lines3D', silent: true, effect: { show: true, trailWidth: 10, trailLength: 0.2, trailOpacity: 1, trailColor: '#fff' } } ],
-		colorMaterial: {
-			textureTiling: 5, //质细节纹理的平铺。默认为1，也就是拉伸填满。大于 1 的时候，数字表示纹理平铺重复的次数。
-		},
+		// colorMaterial: {
+		// 	textureTiling: 5, //质细节纹理的平铺。默认为1，也就是拉伸填满。大于 1 的时候，数字表示纹理平铺重复的次数。
+		// },
 		light: {
 			ambient: {
 				intensity: 0.2,
@@ -62,80 +62,79 @@ export const GEO_3D_OPTIONS = {
 			}
 		],
 	},
-	series: [
-		{
-			type: "lines3D",
-			effect: {
-				show: true,
-				period: 3, // 尾迹特性周期
-				trailLength: 0.1, // 尾迹0~1，线条长度的百分比
-			},
-			lineStyle: {
-				color: "#d9d919",
-				width: 2,
-				opacity: 0.4,
-			},
-			data: [
-				[[114.175193, 22.275706], [120.4574, -1.743046]],
-				[[126.964746, 37.58644], [139.767187, 35.715616]],
-				[[-118.24762, 34.058993], [139.767187, 35.715616]]
-			]
-		},
-		{
-			type: "lines3D",
-			lineStyle: {
-				color: "#fff",
-				width: 2,
-				opacity: 0.6,
-			},
-			// data: this.normalData(this.linesData),
-			data: [
-				[[114.175193, 22.275706], [100.454152, 13.731434]],
-				[[114.175193, 22.275706], [2.332962, 48.864677]],
-				[[101.684555, 3.15663], [114.175193, 22.275706]],
-				[[-118.24762, 34.058993], [151.210446, -33.875774]]
-			]
-		},
-		{
-			type: "scatter3D",
-			coordinateSystem: "globe",
-			zlevel: 3,
-			label: {
-				show: true,
-				position: "bottom",
-				formatter: "{b}",
-				fontSize: 16
-			},
-			itemStyle: {
-				color: "#d9d919",
-			},
-			data: [
-				{ "name": "香港", "value": [114.175193, 22.275706] },
-				{ "name": "东京", "value": [139.767187, 35.715616] },
-				{ "name": "首尔", "value": [126.964746, 37.58644] },
-				{ "name": "印尼", "value": [120.4574, -1.743046] },
-				{ "name": "洛杉矶", "value": [-118.24762, 34.058993] }
-			],
-		},
-		{
-			type: "scatter3D",
-			coordinateSystem: "globe", // 使用的坐标系
-			zlevel: 3,
-			label: {
-				show: true,
-				position: "bottom",
-				formatter: "{b}",
-				fontSize: 16
-			},
-			itemStyle: {
-				color: "#fff",
-			},
-			data: [
-				{ "name": "吉隆坡", "value": [101.684555, 3.15663] },
-				{ "name": "曼谷", "value": [100.454152, 13.731434] },
-				{ "name": "悉尼", "value": [151.210446, -33.875774] },
-				{ "name": "巴黎", "value": [2.332962, 48.864677] }
-			]
-		}
-	]
+	// series: [
+	// 	{
+	// 		type: "lines3D",
+	// 		effect: {
+	// 			show: true,
+	// 			period: 3, // 尾迹特性周期
+	// 			trailLength: 0.1, // 尾迹0~1，线条长度的百分比
+	// 		},
+	// 		lineStyle: {
+	// 			color: "#d9d919",
+	// 			width: 2,
+	// 			opacity: 0.4,
+	// 		},
+	// 		data: [
+	// 			[[114.175193, 22.275706], [120.4574, -1.743046]],
+	// 			[[126.964746, 37.58644], [139.767187, 35.715616]],
+	// 			[[-118.24762, 34.058993], [139.767187, 35.715616]]
+	// 		]
+	// 	},
+	// 	{
+	// 		type: "lines3D",
+	// 		lineStyle: {
+	// 			color: "#fff",
+	// 			width: 2,
+	// 			opacity: 0.6,
+	// 		},
+	// 		// data: this.normalData(this.linesData),
+	// 		data: [
+	// 			[[114.175193, 22.275706], [100.454152, 13.731434]],
+	// 			[[114.175193, 22.275706], [2.332962, 48.864677]],
+	// 			[[101.684555, 3.15663], [114.175193, 22.275706]],
+	// 			[[-118.24762, 34.058993], [151.210446, -33.875774]]
+	// 		]
+	// 	},
+	// 	{
+	// 		type: "scatter3D",
+	// 		coordinateSystem: "globe",
+	// 		zlevel: 3,
+	// 		label: {
+	// 			show: true,
+	// 			position: "bottom",
+	// 			formatter: "{b}",
+	// 			fontSize: 16
+	// 		},
+	// 		itemStyle: {
+	// 			color: "#d9d919",
+	// 		},
+	// 		data: [
+	// 			{ "name": "香港", "value": [114.175193, 22.275706] },
+	// 			{ "name": "东京", "value": [139.767187, 35.715616] },
+	// 			{ "name": "首尔", "value": [126.964746, 37.58644] },
+	// 			{ "name": "印尼", "value": [120.4574, -1.743046] },
+	// 			{ "name": "洛杉矶", "value": [-118.24762, 34.058993] }
+	// 		],
+	// 	},
+	// 	{
+	// 		type: "scatter3D",
+	// 		coordinateSystem: "globe", // 使用的坐标系
+	// 		zlevel: 3,
+	// 		label: {
+	// 			show: true,
+	// 			position: "bottom",
+	// 			fontSize: 16
+	// 		},
+	// 		itemStyle: {
+	// 			color: "#fff",
+	// 		},
+	// 		data: [
+	// 			{ "name": "吉隆坡", "value": [101.684555, 3.15663] },
+	// 			{ "name": "曼谷", "value": [100.454152, 13.731434] },
+	// 			{ "name": "悉尼", "value": [151.210446, -33.875774] },
+	// 			{ "name": "巴黎", "value": [2.332962, 48.864677] }
+	// 		]
+	// 	}
+	// ]
 }
